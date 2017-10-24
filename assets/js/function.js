6415907a8bf177e30566954738fd71586f420bf2
usmonovsardor7770 <usmonovsardor7770@gmail.com>
2017-10-24 15:45:22 -0400

    Coming Soon Page
    Updated HTML, CSS, JS files  removed old files.

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,185 +1,27 @@
-$("#2013").on("click", function() {
-   $.fancybox.open(
-      [{
-            src: "assets/img/2013/1.jpg",
-            opts: {
-               caption: "International Night 2013"
-            }
-         },
-         {
-            src: "assets/img/2013/2.jpg",
-            opts: {
-               caption: "International Night 2013"
-            }
-         }
-      ], {
-         loop: false,
-         hash: "2013",
-         gutter: 50,
-         speed: 400,
-         image: {
-            protect: true
-         }
-      }
-   );
+$(document).ready(function() {
+  $("#loader-wrapper").animate({
+    opacity: 0
+  }, 1000, function() {
+    setTimeout(function() {
+    $('#loader-wrapper').css('display','none');
+  }, 2000);
+  });
 });
-$("#2014").on("click", function() {
-   $.fancybox.open(
-      [{
-            src: "assets/img/2014/1.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/2.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/3.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/4.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/5.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/6.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/7.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/8.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/9.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/10.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         },
-         {
-            src: "assets/img/2014/11.jpg",
-            opts: {
-               caption: "International Night 2014"
-            }
-         }
-      ], {
-         loop: false,
-         hash: "2014",
-         gutter: 50,
-         speed: 400,
-         image: {
-            protect: true
-         }
-      }
-   );
-});
-$("#2015").on("click", function() {
-   $.fancybox.open(
-      [{
-            src: "assets/img/2015/1.jpg",
-            opts: {
-               caption: "International Night 2015"
-            }
-         },
-         {
-            src: "assets/img/2015/2.jpg",
-            opts: {
-               caption: "International Night 2015"
-            }
-         },
-         {
-            src: "assets/img/2015/3.jpg",
-            opts: {
-               caption: "International Night 2015"
-            }
-         },
-         {
-            src: "assets/img/2015/4.jpg",
-            opts: {
-               caption: "International Night 2015"
-            }
-         },
-         {
-            src: "assets/img/2015/5.jpg",
-            opts: {
-               caption: "International Night 2015"
-            }
-         },
-         {
-            src: "assets/img/2015/6.jpg",
-            opts: {
-               caption: "International Night 2015"
-            }
-         },
-         {
-            src: "assets/img/2015/7.jpg",
-            opts: {
-               caption: "International Night 2015"
-            }
-         },
-         {
-            src: "assets/img/2015/8.jpg",
-            opts: {
-               caption: "International Night 2015"
-            }
-         },
 
-      ], {
-         loop: false,
-         hash: "2015",
-         gutter: 50,
-         speed: 400,
-         image: {
-            protect: true
-         }
-      }
-   );
-});
-$("#2016").on("click", function() {
-   $.fancybox.open(
-      [{
-            src: "assets/img/2016/1.jpg",
-            opts: {
-               caption: "International Night 2016"
-            }
-         }
-      ], {
-         loop: false,
-         hash: "2016",
-         gutter: 50,
-         speed: 400,
-         image: {
-            protect: true
-         }
-      }
-   );
+var logo = anime({
+  targets: '#logo',
+  rotate: {
+    value: '1turn',
+    duration: 1800,
+    easing: [.91,-0.54,.29,1.56]
+  },
+  scale: {
+    value: 1.25,
+    duration: 2000,
+    delay: 800,
+    easing: 'easeInOutQuart'
+  },
+  delay: 250,
+  loop: 2,
+  direction: 'alternate'
 });
