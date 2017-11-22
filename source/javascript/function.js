0a82a934908e161e165ceee2aaf65d362f94ca5f
usmonovsardor7770 <usmonovsardor7770@gmail.com>
2017-11-22 15:39:09 -0500

    Cleaned up code for new design

--- source/javascript/function.js
+++ source/javascript/function.js
@@ -1,75 +1,16 @@
 $(document).ready(function() {
 
-  // Logo Animate
-  $("#loader-wrapper").animate({
-    opacity: 0
-  }, 1000, function() {
-    setTimeout(function() {
-      $("#site").fadeIn(500);
-      $('#loader-wrapper').css('display', 'none');
-    }, 2000);
-  });
-
-  // Nav Onscroll add class
-  $(window).scroll(function() {
-    // If scrolled more than 10px
-    if ($(document).scrollTop() > 10) {
-      $("nav").addClass("fixed");
-    } else {
-      if ($("nav").hasClass("fixed")) {
-        $("nav").removeClass("fixed");
-      }
-    }
-  });
-
-  // Hamburger toggler
-  $(".menu").on("click", function() {
-    $(".menu, .overlay").toggleClass("active");
-  });
-  $(".overlay-content > a").on("click", function() {
-    $(".menu, .overlay").removeClass("active");
-  });
-
   // Nav a Onclick scroll to certain id
-  $("a").on("click", function(event) {
-    if (this.hash !== "") {
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
       event.preventDefault();
-
-      var hash = this.hash;
-
-      $("html, body").animate({
-          scrollTop: $(hash).offset().top
-        },
-        1000,
-        function() {
-          window.location.hash = hash;
-        }
-      );
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 700);
     }
   });
 
   // Dynamic Year for footer
-  var d = new Date();
-  var n = d.getFullYear();
-  document.getElementById("year").innerHTML = n;
-});
-
-
-// Animate LOGO
-var logo = anime({
-  targets: '#logo',
-  rotate: {
-    value: '1turn',
-    duration: 1800,
-    easing: [.91,-0.54,.29,1.56]
-  },
-  scale: {
-    value: 1.25,
-    duration: 2000,
-    delay: 800,
-    easing: 'easeInOutQuart'
-  },
-  delay: 250,
-  loop: 2,
-  direction: 'alternate'
+  document.getElementById("year").innerHTML = new Date().getFullYear();
 });
