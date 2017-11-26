b74654f9bd6e76f37c1cfec06133c97507f79940
usmonovsardor7770 <usmonovsardor7770@gmail.com>
2017-11-26 00:42:40 -0500

    Bug Fixes, Added Gallery
    Re-ordered gallery and schedule postion, Added new gallery library & images.

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ gulp.task('js', function(){
 
 // IMG Optimization
 gulp.task('img', function(){
-  return  gulp.src('source/img/**/*')
+  return gulp.src('source/img/**/*')
       .pipe(gulp.dest('public/img'))
       .pipe(browserSync.reload({
          stream: true
--- source/javascript/function.js
+++ source/javascript/function.js
@@ -25,4 +25,6 @@ $(document).ready(function() {
       }
     }
   });
+
+  var lightbox = $('#gallery a').simpleLightbox();
 });
