ce9f83ce1ff2eb411f5f9f011deb73408000bab1
usmonovsardor7770 <usmonovsardor7770@gmail.com>
2018-05-15 10:54:12 -0400

    faster img optomization

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task('js', function () {
 gulp.task('img', function () {
     return gulp.src('source/img/**/*')
         .pipe(image({
-            jpegRecompress: ['--strip', '--quality', 'medium', '--min', 10, '--max', 20],
+            jpegRecompress: ['--strip', '--quality', 'medium', '--min', 5, '--max', 10],
             jpegoptim: false,
             mozjpeg: true,
             concurrent: 10,
